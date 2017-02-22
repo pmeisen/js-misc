9a406af27a35d22246a9d567f813e960d9e3ef93
Philipp Meisen <philipp@meisen.net>
2017-02-21 17:49:29 -0800

    Release v0.0.11

--- gruntfile.js
+++ gruntfile.js
@@ -202,7 +202,7 @@ module.exports = function (grunt) {
 
         bump: {
             options: {
-                // files: ['package.json', 'bower.json'],
+                files: ['package.json', 'bower.json'],
                 commitFiles: ['.'],
                 pushTo: 'origin'
             }
